fbe45ed2609a23ba748e9d37a51cbffb2085705c
Sameer Chawla <sameerchawla@sameers-air.myfiosgateway.com>
2024-01-04 06:02:12 -0500

    created layout page, created sidebar, added icons

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/list")
+def list():
+    return render_template("list.html")
+@app.route("/filter")
+def filter():
+    return render_template("filter.html")
