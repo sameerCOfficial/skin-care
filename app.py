3a77edde178de1e482211780696633c739578b78
Sameer Chawla <sameerchawla@sameers-air.myfiosgateway.com>
2024-01-07 03:49:57 -0500

    Added tables and created the logic for creating them

--- app.py
+++ app.py
@@ -12,26 +12,29 @@ def home():
         skin_type = request.form.get('skin_type')
         
         connection = engine.connect()
-        query = text("SELECT product_type, skin_type, brand, name, price FROM face_product WHERE product_type = :product_type AND skin_type = :skin_type")
-        result = connection.execute(query, {'product_type': product_type, 'skin_type': skin_type})
+        query = text("SELECT product_type, skin_type, brand, name, price FROM face_product WHERE product_type LIKE :product_type AND (skin_type LIKE :skin_type OR skin_type LIKE '%All%')")
+        result = connection.execute(query, {'product_type': f'%{product_type}%', 'skin_type': f'%{skin_type}%'})
 
-        if result.rowcount == 0:  # Check if no rows were returned
+        rows = []
+        for row in result.fetchall():
+            rows.append({'product_type': row[0], 'skin_type': row[1], 'brand': row[2], 'name': row[3], 'price': row[4]})
+        if len(rows) == 0: 
             return render_template('sorry.html')
         else:
-            # Assuming only one row is returned for simplicity
-            row = result.fetchone()
-            product_type = row[0]
-            skin_type = row[1]
-            brand = row[2]
-            name = row[3]
-            price = row[4]
-            return render_template('yourproducts.html', product_type=product_type, skin_type=skin_type, brand=brand, name=name, price=price)
+            return render_template('yourproducts.html', result=rows)
 
     return render_template('index.html')
 
 @app.route("/list")
 def list():
-    return render_template("list.html")
+    connection = engine.connect()
+    query = text("SELECT product_type, skin_type, brand, name, price FROM face_product")
+    result = connection.execute(query)
+
+    rows = []
+    for row in result.fetchall():
+        rows.append({'product_type': row[0], 'skin_type': row[1], 'brand': row[2], 'name': row[3], 'price': row[4]})
+    return render_template("list.html", result = rows)
 
 @app.route("/about")
 def about():
